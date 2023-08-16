ce25fa8bea54d06ae0309f4397ca05d3fea98384
EshitaDevi123321 <133907318+EshitaDevi123321@users.noreply.github.com>
2023-08-16 11:03:02 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+let fan = prompt("Enter the number of fans")
+let light = prompt("Enter the number of lights")
+let fridge =  prompt("Enter the number of fridges")
+
+
+function bill (fan, light, fridge)  {
+
+let totalwat = (fan, light, fridge)
+let wat = totalwat;
+console.log(`your wat is ${wat}`);
+let totalBill = fan + light + fridge;
+let Bill = totalBill;
+console.log(`your bill is ${Bill}`);
+
+
+}
+
+bill( 10000, 25000, 50000);
+wat( 20, 50, 100);
